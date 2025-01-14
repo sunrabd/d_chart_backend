3750a602618c4dbd910c471613147f889de54b44
suraj <surajbombale59@gmail.com>
2025-01-14 12:02:20 +0530

    added chagnes

--- routes/skillpay_route.js
+++ routes/skillpay_route.js
@@ -7,16 +7,18 @@ const { getSkillPaymentDetails } = require('../utils/webhook_controller');
 // Define the route
 router.post('/skill-pay', async (req, res) => {
     try {
-        const {  amount, customer_mobile, customer_email,order_id, user_id} = req.body;
-
+        const { amount, customer_mobile, customer_email, order_id, userId } = req.body;
+     
         const paymentResponse = await createPayment2(
             order_id,
             amount,
             customer_mobile,
             customer_email,
-            user_id
+            userId
         );
 
+
+         console.log(`new reqqqqqqqqqqqqqqq ${userId}`);
         res.status(200).json(paymentResponse);
     } catch (error) {
         res.status(500).json({ status: false, message: 'Payment creation failed', error: error.message });
