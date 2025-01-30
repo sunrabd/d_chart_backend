01c4869be9aea87e207c08830343fa2df17a6c44
surajbombale <surajbombale59@gmail.com>
2025-01-30 15:40:35 +0530

    added changes

--- controllers/load_check_controller.js
+++ controllers/load_check_controller.js
@@ -37,7 +37,7 @@ const createCheckLoad = async (req, res) => {
         }
 
         if (duplicateNumbers.length > 0) {
-            return res.status(200).json({
+            return res.status(201).json({
                 status: true,
                 message: `numbers already exist ${duplicateNumbers.join(', ')}`
             });
