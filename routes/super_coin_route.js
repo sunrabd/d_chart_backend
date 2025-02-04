0dec845165bd9cd9a05a8125aab56a043d8ca075
suraj <babanbombale59@gmail.com>
2025-02-04 10:23:12 +0530

    added changes

--- routes/super_coin_route.js
+++ routes/super_coin_route.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { increaseCoin, deductCoin,getCoinHistory } = require('../controllers/super_coin_controller');
+const { increaseCoin, deductCoin,getCoinHistory ,getAllCoinHistoryToAdmin222} = require('../controllers/super_coin_controller');
+const { route } = require('./market_type_route');
 
 const router = express.Router();
 
@@ -9,5 +10,8 @@ router.post('/increase-coin', increaseCoin);
 router.post('/deduct-coin', deductCoin);
 
 router.get('/history/:userId', getCoinHistory);
+
+
+router.get('/history',getAllCoinHistoryToAdmin222);
 
 module.exports = router;
