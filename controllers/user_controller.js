4af752ea02447a467fb31d7137280cfa687a514c
surajbombale05 <babanbombale59@gmail.com>
2025-02-08 14:13:52 +0530

    added changes

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -504,9 +504,10 @@ exports.getAllUsers = async (req, res) => {
       User.count({ where: { is_active: true, is_paid_member: false, role: 'user' } }),
       User.sum('super_coins'),
     ]);
-    users.forEach(user => {
-      user.createdAt = moment(user.createdAt).tz('Asia/Kolkata').format('YYYY-MM-DD hh:mm:ss A');
-    });
+    const formattedUsers = users.forEach(user => ({
+      ...user.toJSON(),
+      createdAt: moment(user.createdAt).tz('Asia/Kolkata').format('dddd, YYYY-MM-DD hh:mm:ss A')
+    }));
     res.status(200).json({
       status: true,
       message: 'Users retrieved successfully.',
@@ -523,7 +524,7 @@ exports.getAllUsers = async (req, res) => {
         activeButUnpaidUsers,
       },
       totalUserSuperCoinCount: totalUserSuperCoinCount || 0,
-      users,
+      formattedUsers,
     });
   } catch (error) {
     res.status(500).json({ status: false, message: 'Error retrieving users.', error });
