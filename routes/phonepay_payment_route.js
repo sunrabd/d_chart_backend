29f2b8d1e8e711cffd3e9362eb8e44cdff9ab94e
surajbombale <surajbombale59@gmail.com>
2025-07-11 16:26:12 +0530

    added changes

--- routes/phonepay_payment_route.js
+++ routes/phonepay_payment_route.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/phonepay_payment_controller');
 
-router.post('/initiate', controller.initiatePayment);
+router.post('/initiate', controller.createPaymenPhonpe);
 router.get('/status', controller.checkPaymentStatus);
-router.post('/webhook', controller.handleWebhook);
+// router.post('/webhook', controller.handleWebhook);
 
 
 module.exports = router;
